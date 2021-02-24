21c5bcb8b0f6bd3f4848ed0f17b504cf4ca6fd51
janispurins64 <janispurin64@gmail.com>
2021-02-24 16:27:46 +0200

    Zarošanās
    Izveidota nosacījuma zarošanās.

--- static/jp.js
+++ static/jp.js
@@ -1,7 +1,20 @@
 let a;
 var b;
 function jp1(a,b){
-
+    let d;
     console.log("TAs ir kontrolei");
-    console.log(a/b);
+    console.log(b);
+    if(b==0){
+        // operatori TRUE
+        console.log("Ar nulli dalīt nedrīkst");
+    }
+    else{
+        // FALSE
+        console.log(a/b);
+        c = Math.sqrt(a/b);
+        console.log("Kvadrātsakne:");
+        console.log(c);
+    }
+
+
 }
